08f698b7de16d9b1b86447dede7b36e255452289
Timofti Vladimir <vova.timofti7@gmail.com>
2024-09-06 19:08:06 +0300

    Added endpoint for blog post creation

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import status, viewsets
+from rest_framework.decorators import permission_classes
 from rest_framework.generics import GenericAPIView, get_object_or_404
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -16,11 +17,19 @@ class CategoryViewSet(viewsets.ModelViewSet):
 
 class BlogListView(GenericAPIView):
     serializer_class = BlogSerializer
-    permission_classes = (ReadOnly,)
 
+    @permission_classes([ReadOnly])
     def get(self, request: Request) -> Response:
         blogs = Blog.objects.all()
         return Response(self.get_serializer(blogs, many=True).data)
+
+    @permission_classes([AllowAny])
+    def post(self, request: Request) -> Response:
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+        blog = Blog.objects.create(**validated_data)
+        return Response(self.serializer_class(blog).data, status=status.HTTP_201_CREATED)
 
 
 class BlogItemView(GenericAPIView):
