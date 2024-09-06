c02c9cc4cd82daafcc126fbb9398862f4872664b
Timofti Vladimir <vova.timofti7@gmail.com>
2024-09-06 19:11:20 +0300

    Created Comment model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -15,3 +15,8 @@ class Blog(models.Model):
     posted = models.DateField(db_index=True, auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     enabled = models.BooleanField(default=True)
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
