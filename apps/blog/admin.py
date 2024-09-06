90ff343dd02db184c9fe90ad8d02cbf669a8af17
eggy-66 <7385nmo@gmail.com>
2024-09-06 21:21:53 +0300

    task 2 not clear about the real blog name, added BlogAdmin,CategoryAdmin into blog/admin.py

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from apps.blog.models import Blog, Category
 
-admin.site.register(Blog)
-admin.site.register(Category)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'enabled')  # Display blog title and enabled status in the list view
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Blog, BlogAdmin)
+admin.site.register(Category, CategoryAdmin)
