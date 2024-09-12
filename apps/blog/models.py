2d35f9c25efa0d47be1ca5ed36f126236a0661e7
ChetruscaS <chetrucas@bk.ru>
2024-09-12 12:31:57 +0300

    Task 2
    Task 2: Open in Django Admin (access /admin website section) and add in Blog list the real blog name and status ( enabled/disabled)

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -6,7 +6,8 @@ from django.db import models
 class Category(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, db_index=True)
-
+    def __str__(self) -> str:
+        return self.title
 
 class Blog(models.Model):
     title = models.CharField(max_length=255, unique=True)
@@ -15,3 +16,5 @@ class Blog(models.Model):
     posted = models.DateField(db_index=True, auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     enabled = models.BooleanField(default=True)
+    def __str__(self) -> str:
+        return self.title+"   Status="+str( "Enabled" if self.enabled else "Disabled")
