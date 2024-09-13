45a3f35dab9ca40ce889804eedbb56195545cbdb
JCernei <jonnycernei@gmail.com>
2024-09-13 22:27:39 +0300

    Add Comment model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -7,6 +7,9 @@ class Category(models.Model):
     title = models.CharField(max_length=255, db_index=True)
     slug = models.SlugField(max_length=255, db_index=True)
 
+    def __str__(self):
+        return self.title
+
 
 class Blog(models.Model):
     title = models.CharField(max_length=255, unique=True)
@@ -15,3 +18,15 @@ class Blog(models.Model):
     posted = models.DateField(db_index=True, auto_now_add=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     enabled = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="comments")
+    posted = models.DateTimeField(db_index=True, auto_now_add=True)
+
+    def __str__(self):
+        return f"Comment on {self.blog.title.upper()}: {self.text[:50]}"
