96ef6fc85a00c9f6447ed3e3a6e28d9b3f6c44ce
Iulia Tabara <iuliatabara@gmail.com>
2024-09-14 12:28:07 +0300

    [Task 5] Enabled Comments management in Django Admin

--- apps/blog/admin.py
+++ apps/blog/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
-from apps.blog.models import Blog, Category
+from apps.blog.models import Blog, Category, Comments
 
 admin.site.register(Blog)
 admin.site.register(Category)
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ("text", "blog", "id")
+    search_fields = ("text",)
+    list_filter = ("blog",)
